b1b1062ba2b50bd6986e81fba9af5bd03ce607ba
Sergiu <oalasergiu@gmail.com>
2018-06-27 23:37:08 +0200

    use es7 features

--- src/board/move-directions.js
+++ src/board/move-directions.js
@@ -1,28 +1,20 @@
 
 export const oneStepRightMove = {
   check: (coordinates, gridSize) => coordinates.x + 1 < gridSize,
-  getCoordinatesFrom(currentCoordinates) {
-    return { x: currentCoordinates.x + 1, y: currentCoordinates.y };
-  },
+  getCoordinatesFrom = ({ x, y }) => ({ x: x + 1, y: y }),
 };
 
 export const oneStepLeftMove = {
   check: coordinates => coordinates.x > 0,
-  getCoordinatesFrom(currentCoordinates) {
-    return { x: currentCoordinates.x - 1, y: currentCoordinates.y };
-  },
+  getCoordinatesFrom = ({ x, y }) => ({ x: x.x - 1, y: y.y }),
 };
 
 export const oneStepUpMove = {
   check: coordinates => coordinates.y > 0,
-  getCoordinatesFrom(currentCoordinates) {
-    return { x: currentCoordinates.x, y: currentCoordinates.y - 1 };
-  },
+  getCoordinatesFrom = ({ x, y }) => ({ x: x.x, y: y.y - 1 }),
 };
 
 export const oneStepDownMove = {
   check: (coordinates, gridSize) => coordinates.y + 1 < gridSize,
-  getCoordinatesFrom(currentCoordinates) {
-    return { x: currentCoordinates.x, y: currentCoordinates.y + 1 };
-  },
+  getCoordinatesFrom = ({ x, y }) => ({ x: x.x, y: y.y + 1 }),
 };
